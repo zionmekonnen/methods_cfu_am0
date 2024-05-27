f4d74ae5ad7b26f30a6f182980ad51c73b426aff
Artemy Gibson <artemygibson@gmail.com>
2024-05-27 11:40:15 -0700

    Add an example, clarify some instructions

--- final_practice.rb
+++ final_practice.rb
@@ -3,36 +3,47 @@
 # Call each method at least twice and store the return value in a variable. 
 # Use the puts or print command to see the return value in the console.
 
+# Example:
+def laugh
+  "Ha ha ha!!"
+end
 
-# 1: Write a method named greeting that returns a string with a general greeting. 
+first_laugh = laugh 
+last_laugh = laugh 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-
-
-# 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+puts first_laugh
+puts last_laugh
 
 
-# 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+# 1: Write a method named "greeting" that returns a string with a generic greeting. 
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
-# What data type was your argument(s)?
 
 
-# 4: Write a method named square that takes in one integer, and returns the square of that integer.
-# Bonus: Print a sentence that interpolates the return value of your square method.
+# 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
 
 
-# 5: Write a method named check_stock that satisfies the following interaction pattern:
+# 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+
+# What is the return value of your method?
+# How many arguments did you pass your method?
+# What data type was your argument(s)?
+
+
+# 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
+# Bonus: Print a sentence that interpolates the return value of your square method. You should not have to update the method itself to do this.
+
+# What is the return value of your method?
+# How many arguments did you pass your method?
+# What data type was your argument(s)?
+
+
+# 5: Write a method named "check_stock" that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
 check_stock(4, "Coffee");
